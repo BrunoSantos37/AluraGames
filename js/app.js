efd68fb4abfcd8d344a956d938e9f81a2bded667
Bruno Candido dos Santos <bc1620143@gmail.com>
2024-07-07 01:50:10 -0300

    Pequenos desafios e implementações

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
+let jogosAlugados = 0;
 const alterarStatus = (id) => {
      const gameAtual = document.querySelector(`#game-${id}`);
      const capa = gameAtual.querySelector(".dashboard__item__img");
      const botao = gameAtual.querySelector(".dashboard__item__button");
+     const nameGame = gameAtual.querySelector(".dashboard__item__name");
 
-     if (capa.classList.contains("dashboard__item__img--rented")) {
-          capa.classList.remove("dashboard__item__img--rented");
-          botao.classList.remove("dashboard__item__button--return");
-          botao.innerText = "Alugar";
-     } else {
-          capa.classList.add("dashboard__item__img--rented");
-          botao.classList.add("dashboard__item__button--return");
-          botao.innerText = "Devolver";
+     const aceitou = confirm(
+          `Você tem certeza que deseja ${botao.textContent} ${nameGame.textContent}?`
+     );
+
+     if (aceitou) {
+          if (capa.classList.contains("dashboard__item__img--rented")) {
+               capa.classList.remove("dashboard__item__img--rented");
+               botao.classList.remove("dashboard__item__button--return");
+               botao.innerText = "Alugar";
+               jogosAlugados--;
+          } else {
+               capa.classList.add("dashboard__item__img--rented");
+               botao.classList.add("dashboard__item__button--return");
+               botao.innerText = "Devolver";
+               jogosAlugados++;
+          }
      }
+     console.log(`Jogos Alugados : ${jogosAlugados}`);
 };
